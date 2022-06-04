04f4bc084c13c68ff9b424cd1b7aa740ca08d275
Jonathan Racaud <contact@jracaud.com>
2022-06-04 15:21:45 +0200

    Added back the App.dart file

---
+++ monthly_expenses/lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:monthly_expenses/features/report/ui/pages/report_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class App extends StatefulWidget {
+  static Future<SharedPreferences> _preferences =
+      SharedPreferences.getInstance();
+  static Future<SharedPreferences> get preferences {
+    return _preferences;
+  }
+
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Monthly Expenses',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('fr', 'FR'),
+        Locale.fromSubtags(languageCode: 'zh', countryCode: 'CN'),
+        Locale.fromSubtags(languageCode: 'zh', scriptCode: 'Hans'),
+        Locale.fromSubtags(
+            languageCode: 'zh', scriptCode: 'Hans', countryCode: 'CN'),
+      ],
+      home: ReportPage(),
+    );
+  }
+}
--- monthly_expenses/lib/main.dart
+++ monthly_expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:monthly_expenses/app.dart';
 import 'package:niku/niku.dart';
 import 'package:niku/namespace.dart' as n;
 import 'package:device_preview/device_preview.dart';
@@ -7,21 +8,9 @@ import 'package:device_preview/device_preview.dart';
 void main() => runApp(
   DevicePreview(
     enabled: !kReleaseMode,
-    builder: (context) => const MyApp()
+    builder: (context) => App()
   )
 );
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Monthly Expenses",
-      home: ReportPage()
-    );
-  }
-}
 
 class ReportPage extends StatelessWidget {
   const ReportPage({
