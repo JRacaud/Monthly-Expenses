40212ffcf2fa0e0cae04f4d4ac5d646a53317177
Jonathan Racaud <contact@jracaud.com>
2022-06-04 15:13:37 +0200

    Added back common widgets

---
+++ monthly_expenses/lib/common/widgets/action_fab.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ActionFab extends StatelessWidget {
+  const ActionFab({Key? key, this.onPressed, required this.icon})
+      : super(key: key);
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Material(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: theme.colorScheme.secondary,
+      elevation: 4.0,
+      child: IconTheme.merge(
+          data: theme.iconTheme,
+          child: IconButton(onPressed: onPressed, icon: icon)),
+    );
+  }
+}
