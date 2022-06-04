5037fd4b2893abf5bff15f1cb329e2b5f222d2d2
Jonathan Racaud <contact@jracaud.com>
2022-06-04 15:48:53 +0200

    Added back l10n.yaml

--- monthly_expenses/lib/app.dart
+++ monthly_expenses/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:monthly_expenses/features/report/ui/pages/report_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:monthly_expenses/features/report/ui/pages/report_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class App extends StatefulWidget {
